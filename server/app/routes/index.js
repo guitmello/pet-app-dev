2c7e5802a3d645f297b9e2a290abee5c58727a5e
Felipe Reis <coder.felipe@gmail.com>
2018-03-21 11:23:29 -0300

    Update index.js

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
   app.get('/', function(req, res){
-    server.app.controllers.index.home(app, req, res);
+    app.server.app.controllers.index.home(app, req, res);
   });
 };
 

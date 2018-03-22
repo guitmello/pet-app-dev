a0bfe2a354ffdada982dcd687fb89d38cdaf4877
Guilherme de Mello <guillemello@outlook.com>
2018-03-21 21:11:58 -0300

    Alterando index.js da server

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,9 @@
 module.exports = function(app){
   app.get('/', function(req, res){
-    app.server.app.controllers.index.home(app, req, res);
+    // Heroku
+    app.controllers.index.home(app, req, res);
+    // Local
+    //app.server.app.controllers.index.home(app, req, res);
   });
 };
 

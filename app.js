7603956e166507cad6c12572d0647281b4dc35f6
Felipe Reis <coder.felipe@gmail.com>
2018-03-25 05:41:11 -0300

    update porta no console

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = require('./server/config/server');
 const PORT = process.env.PORT || 5000;
 
 // Parametrizar porta de escuta
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`));

24eb0fb8c70f19ad1df18fc169c16c40ef847466
Felipe Reis <coder.felipe@gmail.com>
2018-03-25 05:38:50 -0300

    Refactoring back-end

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+'use strict';
+
 // Importar as configurações do servidor
-var app = require('./server/config/server');
-var PORT = process.env.PORT || 5000;
+const app = require('./server/config/server');
+const PORT = process.env.PORT || 5000;
 
 // Parametrizar porta de escuta
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,9 +1,8 @@
+'use strict';
+
 module.exports = function(app){
   app.get('/', function(req, res){
-    // Heroku
     server.app.controllers.index.home(app, req, res);
-    // Local
-    //app.server.app.controllers.index.home(app, req, res);
   });
 };
 

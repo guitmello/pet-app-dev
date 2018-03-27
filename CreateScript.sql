952b4e5012d043dd916595c573974b2eb687110b
Guilherme de Mello <guillemello@outlook.com>
2018-03-27 02:08:16 -0300

    Update no Script de Criação de Tabelas

--- CreateScript.sql
+++ CreateScript.sql
@@ -1,9 +1,9 @@
-CREATE TABLE ESPECIE (
+CREATE TABLE TB_ESPECIE (
 nm_especie VARCHAR(50),
 cd_especie_pk SERIAL PRIMARY KEY
 );
 
-CREATE TABLE ANIMAL (
+CREATE TABLE TB_ANIMAL (
 cd_animal_pk SERIAL PRIMARY KEY,
 nm_animal VARCHAR(50),
 cd_idade_animal SMALLINT,
@@ -16,22 +16,20 @@ cd_usuario_fk INTEGER,
 FOREIGN KEY(cd_especie_fk) REFERENCES ESPECIE (cd_especie_pk)
 );
 
-CREATE TABLE RACA (
+CREATE TABLE TB_RACA (
 nm_raca VARCHAR(50),
 cd_raca_pk SERIAL PRIMARY KEY
 );
 
-CREATE TABLE MENSAGEM (
+CREATE TABLE TB_MENSAGEM (
 cd_mensagem_pk SERIAL PRIMARY KEY,
 cd_chat_fk INTEGER,
 ds_mensagem VARCHAR(400),
 dt_mensagem DATE
 );
 
-CREATE TABLE USUARIO (
+CREATE TABLE TB_USUARIO (
 nm_tipo_usuario VARCHAR(30),
-nm_rua_usuario VARCHAR(50),
-cd_numero_usuario INTEGER,
 cd_cnpj_usuario BIGINT,
 nm_razao_social_usuario VARCHAR(50),
 nm_usuario VARCHAR(50),
@@ -50,7 +48,7 @@ cd_numero_endereco_usuario SMALLINT,
 ds_complemento_endereco_usuario VARCHAR(50)
 );
 
-CREATE TABLE CHAT (
+CREATE TABLE TB_CHAT (
 cd_chat_pk SERIAL PRIMARY KEY,
 cd_usuario_enviou_fk INTEGER,
 cd_usuario_recebeu_fk INTEGER,
@@ -60,6 +58,6 @@ FOREIGN KEY(cd_usuario_enviou_fk) REFERENCES USUARIO (cd_usuario_pk),
 FOREIGN KEY(cd_usuario_recebeu_fk) REFERENCES USUARIO (cd_usuario_pk)
 );
 
-ALTER TABLE ANIMAL ADD FOREIGN KEY(cd_raca_fk) REFERENCES RACA (cd_raca_pk);
-ALTER TABLE ANIMAL ADD FOREIGN KEY(cd_usuario_fk) REFERENCES USUARIO (cd_usuario_pk);
-ALTER TABLE MENSAGEM ADD FOREIGN KEY(cd_chat_fk) REFERENCES CHAT (cd_chat_pk);
+ALTER TABLE TB_ANIMAL ADD FOREIGN KEY(cd_raca_fk) REFERENCES TB_RACA (cd_raca_pk);
+ALTER TABLE TB_ANIMAL ADD FOREIGN KEY(cd_usuario_fk) REFERENCES TB_USUARIO (cd_usuario_pk);
+ALTER TABLE TB_MENSAGEM ADD FOREIGN KEY(cd_chat_fk) REFERENCES TB_CHAT (cd_chat_pk);

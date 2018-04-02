2b4e17cc0112f44549c854921c92c39659575221
Guilherme de Mello <guillemello@outlook.com>
2018-04-01 23:11:28 -0300

    Update - Tela Meus-Pets

--- client/src/app/meus-pets/meus-pets.component.ts
+++ client/src/app/meus-pets/meus-pets.component.ts
@@ -22,6 +22,14 @@ export class MeusPetsComponent implements OnInit {
       ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'Alberto' },
       { id: 2, nm_animal: 'Pit', cd_idade_animal: '4 anos', nm_raca_animal: 'Pincher',
       ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'Pedro' },
+      { id: 3, nm_animal: 'Pie', cd_idade_animal: '3 anos', nm_raca_animal: 'Desconhecida',
+      ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'Junior' },
+      { id: 4, nm_animal: 'Sapeca', cd_idade_animal: '7 anos', nm_raca_animal: 'Pincher',
+      ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'João' },
+      { id: 5, nm_animal: 'Salazar', cd_idade_animal: '9 anos', nm_raca_animal: 'Vira-Lata',
+      ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'José' },
+      { id: 6, nm_animal: 'Jr', cd_idade_animal: '5 anos', nm_raca_animal: 'Pincher',
+      ds_foto_animal: '../../assets/images/ft-pet.jpg', nm_usuario_fk: 'Maria' },
     ];
   }
 

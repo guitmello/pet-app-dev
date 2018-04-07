091d4fdf6933be248b43a37e238381692e229716
Felipe Reis <coder.felipe@gmail.com>
2018-04-07 01:30:14 -0300

    Redirecionamento da rota para home

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,4 +4,8 @@ module.exports = function(app){
   app.get('/', function(req, res){
     server.app.controllers.index.home(app, req, res);
   });
+
+  app.get('/*', function(req, res) {
+    res.redirect('/');
+ });
 };

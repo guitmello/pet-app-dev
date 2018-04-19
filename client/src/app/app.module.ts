0b5d19ed87595e320a8b3b468497b2b63ded51e4
Guilherme de Mello <guillemello@outlook.com>
2018-04-19 00:36:57 -0300

    Requisição do usuário

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -35,7 +35,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({

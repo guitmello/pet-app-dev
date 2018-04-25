ab4f722dfda977f8495bdea2be223fc26c688fb3
Murilo Medeiros <murilo_medeiros98@hotmail.com>
2018-04-24 21:35:43 -0300

    Cadastro de pet, parte front feita. Post feito, porém comentado, precisa fazer o mockable

--- client/src/app/add-pet/add-pet.component.ts
+++ client/src/app/add-pet/add-pet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pet } from './pet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-pet',
@@ -10,29 +12,47 @@ export class AddPetComponent implements OnInit {
   sexo: Array<any>;
   especie: Array<any>;
   raca: Array<any>;
+  public urlPet: string;
 
-  constructor() { }
+  pet: Pet = new Pet();
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.sexo = [
-      {value: 'Masculino', viewValue: 'Masculino'},
-      {value: 'Feminino', viewValue: 'Feminino'}
+      { value: 'M', viewValue: 'Masculino' },
+      { value: 'F', viewValue: 'Feminino' }
     ];
 
     this.especie = [
-      {value: 'Espécie 1', viewValue: 'Espécie 1'},
-      {value: 'Espécie 2', viewValue: 'Espécie 2'},
-      {value: 'Espécie 3', viewValue: 'Espécie 3'},
-      {value: 'Espécie 4', viewValue: 'Espécie 4'},
-      {value: 'Espécie 5', viewValue: 'Espécie 5'}
+      { id: 1, value: 'Espécie 1' },
+      { id: 2, value: 'Espécie 2' },
+      { id: 3, value: 'Espécie 3' },
+      { id: 4, value: 'Espécie 4' },
+      { id: 5, value: 'Espécie 5' }
     ];
 
     this.raca = [
-      {value: 'Raça 1', viewValue: 'Raça 1'},
-      {value: 'Raça 2', viewValue: 'Raça 2'},
-      {value: 'Raça 3', viewValue: 'Raça 3'},
-      {value: 'Raça 4', viewValue: 'Raça 4'}
+      { id: 1, value: 'Raça 1' },
+      { id: 2, value: 'Raça 2' },
+      { id: 3, value: 'Raça 3' },
+      { id: 4, value: 'Raça 4' }
     ];
   }
+
+  registerPet() {
+    console.log(this.pet);
+    // this.urlPet = 'http://demo5541414.mockable.io/add-pet%3Ftype=sucess';
+    // return this.httpClient.post<Pet>(this.urlPet, this.pet)
+    //   .subscribe(
+    //     res => {
+    //       console.log(res);
+    //     },
+    //     err => {
+    //       console.log("Error occured");
+    //     }
+    //   );
+  }
+
 
 }

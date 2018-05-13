2cc0cd7a64006d97f9dfe1de22f1e16e91e3d9df
GUILHERME DE MELLO <guilherme.mello01@fatec.sp.gov.br>
2018-05-13 19:10:06 -0300

    Foto do animal funcionando, Faltando arrumar o onload

--- client/src/app/meus-pets/meus-pets.component.ts
+++ client/src/app/meus-pets/meus-pets.component.ts
@@ -1,38 +1,47 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
-import { NgModel } from '@angular/forms';
-import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { MeusPets } from './meus-pets';
-import { ModalDeletePetComponent } from './modal-delete-pet/modal-delete-pet.component';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { Component, OnInit, EventEmitter, Input, Output } from "@angular/core";
+import { NgModel } from "@angular/forms";
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { MeusPets } from "./meus-pets";
+import { ModalDeletePetComponent } from "./modal-delete-pet/modal-delete-pet.component";
+import { MatDialog, MatDialogRef } from "@angular/material";
 
 const api_url = environment.apiUrl;
 
 @Component({
-  selector: 'app-meus-pets',
-  templateUrl: './meus-pets.component.html',
-  styleUrls: ['./meus-pets.component.css']
+  selector: "app-meus-pets",
+  templateUrl: "./meus-pets.component.html",
+  styleUrls: ["./meus-pets.component.css"]
 })
-
 export class MeusPetsComponent implements OnInit {
   private apiUrl = api_url;
   dataPets: any = {};
   dataRacas: any = {};
   dataEspecies: any = {};
-  pets: any = {};
+  pets: Array<any>;
   racas: Array<any>;
   especies: Array<any>;
 
   deleteId: number;
 
-  constructor(private httpClient: HttpClient, public dialog: MatDialog,
-  private meusPets: MeusPets, public router: Router) {}
+  constructor(
+    private httpClient: HttpClient,
+    public dialog: MatDialog,
+    private meusPets: MeusPets,
+    public router: Router
+  ) {}
 
   async ngOnInit() {
-    await this.getRacas('/api/racas/all');
-    await this.getEspecies('/api/especies/all');
-    this.getMeusPets('/api/animals/myanimals/' + localStorage.getItem('id'));
+    //await this.getRacas("/api/racas/all");
+    //await this.getEspecies("/api/especies/all");
+    this.getMeusPets("/api/animals/myanimals/" + localStorage.getItem("id"));
+  }
+
+  imgLoad(id, img) {
+    document.getElementById(id).style.backgroundImage = "url(" + img + ")";
+    console.log(id);
+    console.log(img);
   }
 
   getRacas(url: string) {
@@ -52,38 +61,22 @@ export class MeusPetsComponent implements OnInit {
   }
 
   getMeusPets(url: string) {
-    const userToken = localStorage.getItem('token');
-    const headers = new HttpHeaders().set('Authorization', userToken);
+    const userToken = localStorage.getItem("token");
+    const headers = new HttpHeaders().set("Authorization", userToken);
 
-    this.httpClient.get(api_url + url, { headers }).subscribe( data => {
+    this.httpClient.get(api_url + url, { headers }).subscribe(data => {
       this.dataPets = data;
       this.pets = this.dataPets.payload;
-      this.pets.forEach(element => {
-        for (let x = 0; x <= this.racas.length - 1; x++) {
-          if ( element.id_raca === this.racas[x].id_raca ) {
-            element.nm_raca = this.racas[x].nm_raca;
-            this.pets.nm_raca = element.nm_raca;
-          }
-        }
-
-        for (let y = 0; y <= this.especies.length - 1; y++) {
-          if (element.id_especie === this.especies[y].id_especie) {
-            element.nm_especie = this.especies[y].nm_especie;
-            this.pets.nm_especie = element.nm_especie;
-          }
-        }
-      });
     });
   }
 
   openDialog(id: number) {
     const dialogRef = this.dialog.open(ModalDeletePetComponent, {
-      width: '300px',
-      height: '210px',
+      width: "300px",
+      height: "210px",
       data: {
         id: id
       }
     });
   }
-
 }
